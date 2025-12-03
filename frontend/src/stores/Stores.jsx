107a47fa2ab49652be51b2c8b77b2f71211ffdb0
Anjali1684 <purohitanjali098@gmail.com>
2025-12-03 11:18:11 +0530

    fix: navbar

--- frontend/src/stores/Stores.jsx
+++ frontend/src/stores/Stores.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../component/navbar'
+import Navbar from '../component/Navbar'
 import Store from '../component/Store'
 import Footer from '../component/Footer'
 
